a94ef268ece7857b7d005c89e042ef3a4bc2408c
sdurutr436 <sdurutr436@g.educaand.es>
2026-07-06 11:33:58 +0200

    first commit

---
+++ backend/src/main/java/practikalia/PractikaliaApplication.java
@@ -0,0 +1,13 @@
+package practikalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractikaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractikaliaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/practikalia/PractikaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package practikalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractikaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
